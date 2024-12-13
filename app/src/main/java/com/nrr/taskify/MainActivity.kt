5c61a83426084295334cb9bf109782d4741ddc84
kite1412 <frozradit@gmail.com>
2024-12-13 22:56:18 +0700

    Update app's default font

--- app/src/main/java/com/nrr/taskify/MainActivity.kt
+++ app/src/main/java/com/nrr/taskify/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.nrr.designsystem.theme.TaskifyTheme
 
@@ -32,10 +35,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(modifier = modifier) {
+        Text(text = "Hello $name!")
+        Text(
+            text = "Hello $name!",
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Hello $name!",
+            fontWeight = FontWeight.Bold,
+            fontStyle = FontStyle.Italic
+        )
+    }
 }
 
 @Preview(showBackground = true)
--- core/designsystem/src/main/java/com/nrr/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/nrr/designsystem/theme/Type.kt
@@ -2,14 +2,20 @@ package com.nrr.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.nrr.designsystem.R
+
+val Comfortaa = FontFamily(
+    Font(resId = R.font.comfortaa_variablefont)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Comfortaa,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
