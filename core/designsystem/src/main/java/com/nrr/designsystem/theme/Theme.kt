fb2d23629c65b3f218f7c60ea8d86af683650ca8
kite1412 <frozradit@gmail.com>
2024-12-14 14:46:42 +0700

    Update app color schemes

--- core/designsystem/src/main/java/com/nrr/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/nrr/designsystem/theme/Theme.kt
@@ -11,25 +11,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = VelvetRose,
+    secondary = SpinachWhite,
+    tertiary = SpinachWhite,
+    background = SpinachWhite,
+    onBackground = CilantroCream,
+    surface = CilantroCream
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = VelvetRose,
+    secondary = CharcoalClay,
+    tertiary = CharcoalClay,
+    background = CharcoalClay,
+    onBackground = CharcoalClay30,
+    surface = CharcoalClay30
 )
 
 @Composable
