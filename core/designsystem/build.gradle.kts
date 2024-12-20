5a2e600ebacf7d0a10ac03522d9944a637357bb0
kite1412 <frozradit@gmail.com>
2024-12-20 10:16:09 +0700

    Add Material 3 Adaptive library to designsystem module

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -14,4 +14,5 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
     api(libs.androidx.compose.material3)
+    api(libs.androidx.adaptive)
 }
