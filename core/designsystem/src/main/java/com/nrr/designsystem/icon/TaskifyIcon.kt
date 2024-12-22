73adefde96007f90f503f2bf0aa549d3bf19b526
kite1412 <frozradit@gmail.com>
2024-12-22 17:52:08 +0700

    Update assets

--- core/designsystem/src/main/java/com/nrr/designsystem/icon/TaskifyIcon.kt
+++ core/designsystem/src/main/java/com/nrr/designsystem/icon/TaskifyIcon.kt
@@ -3,10 +3,15 @@ package com.nrr.designsystem.icon
 import com.nrr.designsystem.R
 
 object TaskifyIcon {
-    val appIcon: Int = R.drawable.app_icon
-    val home: Int = R.drawable.home
-    val profile: Int = R.drawable.profile
-    val note: Int = R.drawable.note
-    val setting: Int = R.drawable.setting
-    val chart: Int = R.drawable.chart
+    val appIcon = R.drawable.app_icon
+    val home = R.drawable.home
+    val profile = R.drawable.profile
+    val note = R.drawable.note
+    val setting = R.drawable.setting
+    val chart = R.drawable.chart
+    val heartPulse = R.drawable.heart_pulse
+    val workCase = R.drawable.work_case
+    val book = R.drawable.book
+    val pray = R.drawable.pray
+    val star = R.drawable.star
 }
