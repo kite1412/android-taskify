69bba1fe838d4c75666d5530bc4244e439f2d30f
kite1412 <frozradit@gmail.com>
2024-12-26 10:19:47 +0800

    Add utility object for retrieving string resource IDs

---
+++ feature/todayplan/src/main/java/com/nrr/todayplan/util/TodayPlanDictionary.kt
@@ -0,0 +1,15 @@
+package com.nrr.todayplan.util
+
+import com.nrr.todayplan.R
+
+object TodayPlanDictionary {
+    val greeting = R.string.todayplan_greeting
+    val question = R.string.todayplan_question
+    val planForToday = R.string.todayplan_plan_for_today
+    val todayProgress = R.string.todayplan_today_progress
+    val completed = R.string.todayplan_completed
+    val tasksLeft = R.string.todayplan_tasks_left
+    val weekly = R.string.todayplan_weekly
+    val monthly = R.string.todayplan_monthly
+    val schedule = R.string.todayplan_schedule
+}
