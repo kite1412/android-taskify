f3135bf7c909cf5486a8e4fcbea6e5f82219b573
kite1412 <frozradit@gmail.com>
2024-12-26 12:29:44 +0800

    Create PlanForToday

--- feature/todayplan/src/main/java/com/nrr/todayplan/TodayPlanScreen.kt
+++ feature/todayplan/src/main/java/com/nrr/todayplan/TodayPlanScreen.kt
@@ -1,6 +1,8 @@
 package com.nrr.todayplan
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +11,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,8 +29,10 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.nrr.designsystem.component.AdaptiveText
+import com.nrr.designsystem.icon.TaskifyIcon
 import com.nrr.designsystem.theme.PastelGreen
 import com.nrr.designsystem.theme.TaskifyTheme
+import com.nrr.designsystem.theme.lightOrangeGradient
 import com.nrr.todayplan.util.TodayPlanDictionary
 
 @Composable
@@ -93,5 +100,44 @@ private fun GreetingHeaderPreview() {
             username = "Kite1412",
             modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp)
         )
+    }
+}
+
+@Composable
+internal fun PlanForToday(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .background(
+                brush = Brush.linearGradient(
+                    colors = lightOrangeGradient
+                )
+            )
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(TaskifyIcon.calendar3d),
+            contentDescription = "calendar illustration",
+            modifier = Modifier.size(40.dp)
+        )
+        Text(
+            text = stringResource(TodayPlanDictionary.planForToday),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PlanForTodayPreview() {
+    TaskifyTheme {
+        PlanForToday {}
     }
 }
