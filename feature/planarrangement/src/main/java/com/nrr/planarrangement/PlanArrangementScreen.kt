cfec906b1db37911eacf27cc65cfdd943129f18d
kite1412 <frozradit@gmail.com>
2025-01-11 14:40:50 +0800

    Create Header

--- feature/planarrangement/src/main/java/com/nrr/planarrangement/PlanArrangementScreen.kt
+++ feature/planarrangement/src/main/java/com/nrr/planarrangement/PlanArrangementScreen.kt
@@ -1,12 +1,86 @@
 package com.nrr.planarrangement
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nrr.designsystem.component.AdaptiveText
+import com.nrr.designsystem.icon.TaskifyIcon
+import com.nrr.designsystem.theme.TaskifyTheme
+import com.nrr.designsystem.util.TaskifyDefault
+import com.nrr.planarrangement.util.PlanArrangementDictionary
 
 @Composable
 internal fun PlanArrangementScreen(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    Content(
+        onBackClick = onBackClick,
+        modifier = modifier
+    )
+}
 
+@Composable
+private fun Content(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        Header(
+            onBackClick = onBackClick
+        )
+    }
+}
+
+@Composable
+private fun Header(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        IconButton(
+            onClick = onBackClick
+        ) {
+            Icon(
+                painter = painterResource(TaskifyIcon.back),
+                contentDescription = "back"
+            )
+        }
+        AdaptiveText(
+            text = stringResource(PlanArrangementDictionary.arrangePlan),
+            initialFontSize = TaskifyDefault.HEADER_FONT_SIZE.sp,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ContentPreview() {
+    TaskifyTheme {
+        Content(
+            onBackClick = {}
+        )
+    }
 }
---
+++ feature/planarrangement/src/main/java/com/nrr/planarrangement/util/PlanArrangementDictionary.kt
@@ -0,0 +1,7 @@
+package com.nrr.planarrangement.util
+
+import com.nrr.planarrangement.R
+
+object PlanArrangementDictionary {
+    val arrangePlan = R.string.planarrangement_arrange_plan
+}
