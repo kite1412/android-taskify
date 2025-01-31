ea3a67a5569d18f6bfd421d1e65467ad877e4a02
kite1412 <frozradit@gmail.com>
2025-01-31 12:00:14 +0800

    Add hilt android compiler on summaries

--- summary/build.gradle.kts
+++ summary/build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
 
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.hilt.work)
+    ksp(libs.hilt.android.compiler)
 
     androidTestImplementation(libs.androidx.work.testing)
     androidTestImplementation(libs.kotlinx.coroutines.test)
