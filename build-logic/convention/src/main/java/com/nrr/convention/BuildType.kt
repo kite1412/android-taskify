192953efc3de5091a62f8a14a30ad9c9ff153eb7
kite1412 <frozradit@gmail.com>
2025-04-26 22:02:21 +0700

    Update debug variant config

--- build-logic/convention/src/main/java/com/nrr/convention/BuildType.kt
+++ build-logic/convention/src/main/java/com/nrr/convention/BuildType.kt
@@ -22,6 +22,10 @@ internal fun Project.configureBuildTypes(
                                 commonExtension = commonExtension
                             )
                         }
+                        debug {
+                            applicationIdSuffix = ".debug"
+                            versionNameSuffix = "-debug"
+                        }
                     }
                 }
             }
