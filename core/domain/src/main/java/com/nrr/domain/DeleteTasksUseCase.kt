2bfab46f77a831818fc9c4e364522cf3d240a279
kite1412 <frozradit@gmail.com>
2025-05-04 20:18:56 +0700

    Delete all associated reminders of a deleted task

--- core/domain/src/main/java/com/nrr/domain/DeleteTasksUseCase.kt
+++ core/domain/src/main/java/com/nrr/domain/DeleteTasksUseCase.kt
@@ -2,11 +2,20 @@ package com.nrr.domain
 
 import com.nrr.data.repository.TaskRepository
 import com.nrr.model.Task
+import com.nrr.notification.ScheduledTaskNotifier
 import javax.inject.Inject
 
 class DeleteTasksUseCase @Inject constructor(
-    private val taskRepository: TaskRepository
+    private val taskRepository: TaskRepository,
+    private val scheduledTaskNotifier: ScheduledTaskNotifier
 ) {
-    suspend operator fun invoke(tasks: List<Task>) =
+    suspend operator fun invoke(tasks: List<Task>) {
         taskRepository.deleteTasks(tasks)
+
+        tasks.forEach {
+            it.activeStatuses.forEach { s ->
+                if (s.reminderSet) scheduledTaskNotifier.cancelReminder(it)
+            }
+        }
+    }
 }
